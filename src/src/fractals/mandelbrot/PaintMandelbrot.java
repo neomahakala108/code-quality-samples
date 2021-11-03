e55d0fbe23e75a30e49e2e75a04425a45d0df0ad
nm108 <neomahakala108@gmail.com>
2021-11-03 17:31:10 +0100

    .gitignore commit & push
    PaintMandelbrot - removed unneeded import.

--- src/src/fractals/mandelbrot/PaintMandelbrot.java
+++ src/src/fractals/mandelbrot/PaintMandelbrot.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
-import javax.swing.JFileChooser;
 
 public class PaintMandelbrot {
 	public static void main(String[] args) throws Exception {
